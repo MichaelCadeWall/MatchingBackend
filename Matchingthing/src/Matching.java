c944a13900cc6caaacc78cb35c18859508371681
Tyrannosuarez9 <andres_suarez05@yahoo.com>
2018-11-14 21:41:06 -0600

    matching update

--- Matchingthing/src/Matching.java
+++ Matchingthing/src/Matching.java
@@ -3,22 +3,18 @@ import java.util.ArrayList;
 
 public class Matching {
 		ConnectionMaker forcreation = new ConnectionMaker();
-		//Matching useguy=new Matching();//not populated used for method calls
+
+	
 	private Rider getriderDestination(int input) {
 		java.sql.Connection con = forcreation.MakeConnection();
 		Rider returnthing = new Rider();
 		try {
 			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("select * from riderpost WHERE postID = "+input);
+			ResultSet rs = stmt.executeQuery("select * from riderpost WHERE postID in (select major from profile where idProfile=)"+input);
 			
-			if(rs.next()){
+			rs.next();
 			returnthing.destination=rs.getString("DestinationID");
 			returnthing.riderId=rs.getInt("RiderID");
-			//System.out.println( "getriderdestination debug");
-			//System.out.println(returnthing.destination + " + " + rs.getString("DestinationID"));
-			}
-			else 
-				return null;
 			
 		} catch(SQLException e) {
 			e.printStackTrace();
@@ -26,30 +22,30 @@ public class Matching {
 		try {
 			con.close();
 		}catch (SQLException e) {
-			//System.out.println("get Rider Destination failed to close connection");
+			System.out.println("get Rider Destination failed to close connection");
 			e.printStackTrace();
 		}
 		return returnthing;
 	}
 	
-	ArrayList<Driver> getdrivers(Rider stats){
+	ArrayList<Driver> getdrivers(String destination, String date){
 		java.sql.Connection con = forcreation.MakeConnection();
 		
 		
 		ArrayList<Driver> returninglist = new ArrayList<Driver>();
 		
 		
-		String query = "select * from driverpost where DestinationID='" + stats.destination+"'" +"AND Date="+stats.date+"";
-		if(stats.destination==null)
-			return null;
-		//System.out.println(query);
+		String query = "select * from driverpost where DestinationID='" + destination+"' and Date =" +date;
+		if(destination==null)
+			return returninglist;
+		System.out.println(query);
 		try {
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
 			while(rs.next()) {
 				Driver temp=new Driver();
 				temp.date=rs.getString("Date");
-				temp.destination=rs.getString("DestinationID");
+				temp.destination=destination;
 				temp.driverId=rs.getInt("driverID");
 				temp.seats=rs.getInt("Seats");
 				temp.time=rs.getString("Time");
@@ -58,7 +54,7 @@ public class Matching {
 				returninglist.add(temp);
 			}
 		} catch (SQLException e) {
-			//System.out.println("get driverpost failed");
+			System.out.println("get driverpost failed");
 			e.printStackTrace();
 		}
 		return returninglist;
@@ -67,39 +63,26 @@ public class Matching {
 	 boolean Match(int postid) {	
 		Matching useguy=new Matching();
 		//java.sql.Connection con = forcreation.MakeConnection();
-		Rider rider=new Rider();
-		rider = useguy.getriderDestination(postid);
-		if (rider==null) //if that postid has already been matched;
-			return true;
-		
-		//System.out.println(rider.destination);
-		ArrayList<Driver> driver = useguy.getdrivers(rider);
+		Rider rider= useguy.getriderDestination(postid) ;
+		ArrayList<Driver> driver = useguy.getdrivers(rider.destination, rider.date);
 		if (driver.isEmpty())
 			return false;
 		Driver guy= driver.get(0);
 		try {
-			//System.out.println(guy.destination);
-		insert(guy.tripId,rider.riderId, guy.driverId, guy.destination);
+			System.out.println(guy.destination);
+		insert(guy.tripId,rider.riderId, guy.driverId, guy.destination, guy.date);
 		return true;
 		}catch(Exception e) {
 			return false;
 		}
 	}
 	
-	private void insert(int tripID, int riderID, int driverID, String destination) {
+	private void insert(int tripID, int riderID, int driverID, String destination, String Date) {
 		java.sql.Connection con = forcreation.MakeConnection();
-//		
-//		Rider rider;
-//		Driver driver;
-//		
-//		destination = driver.destination;
-//		riderID = rider.riderId;
-//		driverID = driver.driverId;
-//		tripID = driver.tripId;
-//		
+
 	
-		String sql = "insert into matching(TripID, DriverID, RiderID, DestinationID) VALUES (?,?,?,?)";
-		//System.out.println(sql);
+		String sql = "insert into matching(TripID, DriverID, RiderID, DestinationID, Date) VALUES (?,?,?,?,?)";
+		System.out.println(sql);
 		
 		try (
 			PreparedStatement pstmt = con.prepareStatement(sql)){
@@ -107,6 +90,7 @@ public class Matching {
 			pstmt.setInt(3, riderID);
 			pstmt.setInt(2, driverID);
 			pstmt.setString(4, destination);
+			pstmt.setString(5, Date);
 			pstmt.executeUpdate();
 		} catch (SQLException e) {
 		System.out.println(e.getMessage());
